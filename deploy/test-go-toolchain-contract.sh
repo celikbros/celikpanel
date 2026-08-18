d6c1ed5a769ee0b9508048103f325a0b4f56af2a
celikbros <62828186+celikbros@users.noreply.github.com>
2026-08-19 02:47:22 +0300

    ci: split overloaded panel race shard

--- deploy/test-go-toolchain-contract.sh
+++ deploy/test-go-toolchain-contract.sh
@@ -133,8 +133,9 @@ reject_literal "$CI" 'windows-portability:'
 reject_literal "$CI" 'freebsd-compile:'
 reject_literal "$CI" 'darwin-compile:'
 require_count "$CI" 'go test -race -count=1 -timeout=8m ./cmd/panel' 1
-require_count "$CI" 'pattern:' 5
-require_literal "$CI" "pattern: '^Test($|[A-G]|[^A-Z])'"
+require_count "$CI" 'pattern:' 6
+require_literal "$CI" "pattern: '^Test($|[A-C]|[^A-Z])'"
+require_literal "$CI" "pattern: '^Test[D-G]'"
 require_literal "$CI" "pattern: '^Test[H-M]'"
 require_literal "$CI" "pattern: '^Test[N-R]'"
 require_literal "$CI" "pattern: '^TestS'"
@@ -163,7 +164,8 @@ reject_literal "$PORTABILITY" 'go-version-file:'
 # sufficient proof that the empty suffix remains covered.
 panel_shard_membership_count() {
     local test_name=$1 count=0
-    [[ "$test_name" =~ ^Test($|[A-G]|[^A-Z]) ]] && ((count += 1))
+    [[ "$test_name" =~ ^Test($|[A-C]|[^A-Z]) ]] && ((count += 1))
+    [[ "$test_name" =~ ^Test[D-G] ]] && ((count += 1))
     [[ "$test_name" =~ ^Test[H-M] ]] && ((count += 1))
     [[ "$test_name" =~ ^Test[N-R] ]] && ((count += 1))
     [[ "$test_name" =~ ^TestS ]] && ((count += 1))
@@ -171,7 +173,7 @@ panel_shard_membership_count() {
     printf '%s\n' "$count"
 }
 
-for boundary_name in Test TestA TestG TestH TestM TestN TestR TestS TestT TestZ Test_ Test9; do
+for boundary_name in Test TestA TestC TestD TestG TestH TestM TestN TestR TestS TestT TestZ Test_ Test9 Testa; do
     [[ "$(panel_shard_membership_count "$boundary_name")" == 1 ]] ||
         die "panel race shard boundary is not disjoint and exhaustive: $boundary_name"
 done
